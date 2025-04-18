4de58235abc78d5ad2723a26504252874eb9621a
ShadowWalker2014 <kai.jiabo.feng@gmail.com>
2025-04-18 04:58:40 +0000

    Fix Edge Function Import Map

--- supabase/functions/replicate/index.ts
+++ supabase/functions/replicate/index.ts
@@ -3,7 +3,13 @@
 // https://deno.land/manual/examples/deploy_node_server
 
 import { serve } from "https://deno.land/std@0.168.0/http/server.ts"
-import { corsHeaders } from "../_shared/cors.ts"
+
+// CORS headers for browser compatibility
+const corsHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Headers": "authorization, x-client-info, apikey, content-type",
+  "Access-Control-Allow-Methods": "POST, OPTIONS",
+}
 
 interface ReplicateRequest {
   apiKey: string;
